47a24aeaaa8714a78d18df804e78ad146bd0ec94
xaizek <xaizek@posteo.net>
2023-03-05 16:45:39 +0200

    Make text on a badge more legible via better fonts

--- badge.php
+++ badge.php
@@ -55,7 +55,10 @@ try {
         <rect rx=\"3\" x=\"37\" width=\"53\" height=\"20\" fill=\"$color\"/>
         <path fill=\"$color\" d=\"M37 0h4v20h-4z\"/>
         <rect rx=\"3\" width=\"90\" height=\"20\" fill=\"url(#a)\"/>
-        <g fill=\"#fff\" text-anchor=\"middle\" font-family=\"sans-serif\" font-size=\"11\">
+        <g fill=\"#fff\"
+           text-anchor=\"middle\"
+           font-family=\"DejaVu Sans,Verdana,Geneva,sans-serif\"
+           font-size=\"11\">
             <text x=\"19.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">build</text>
             <text x=\"19.5\" y=\"14\">build</text>
             <text x=\"62.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">$label</text>
